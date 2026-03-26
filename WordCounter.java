8476978ffc0c68da07ea8649349b018880050659
shreesagne2-lgtm <shreesagne2@gmail.com>
2026-03-26 23:26:21 +0530

    program3.6

---
+++ WordCounter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class WordCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(sentence);
+
+        int count = 0;
+        while (st.hasMoreTokens()) {
+            st.nextToken();
+            count++;
+        }
+
+        System.out.println("Total number of words: " + count);
+    }
+}
